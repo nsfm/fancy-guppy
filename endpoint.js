d3bfcd619ad3846010b909cb5c66a464a651ceed
Nate Dube <nate@ndube.com>
2019-03-07 00:34:57 -0500

    Lint config changes and some documentation.

--- endpoint.js
+++ endpoint.js
@@ -2,13 +2,103 @@
 
 const { authenticators } = require('fancy-guppy/authentication.js');
 
-// This class provides a common interface to each endpoint so they can be configured consistently and
-// automatically.
+/**
+ * This class provides a common interface to each endpoint so they can be configured consistently and
+ * automatically.
+ */
 class Endpoint {
+  /**
+   *  Accepts basic endpoint configuration.
+   *
+   *  @param {object} server An initialized Express server.
+   *  @param {object} database An initialized Sequelize handle.
+   *  @param {object} logger An initialized Bunyan handle.
+   *  @param {config} config Additional basic configuration options and endpoint-specific parameters.
+   */
+  constructor(server, database, logger, config) {
+    // Make these handles accessible.
+    this.server = server;
+    this.database = database;
+    this.models = database.models;
+    this.Op = database.sequelize.Op;
+    this.log = logger ? logger.child(__filename) : console;
+
+    // Default configuration values.
+    this.method = 'get';
+    this.path = '';
+    this.scopes = [];
+    this.transaction = false;
+    this.authenticator = 'none';
+    this.request_schemas = []; // An array of maps to fields in the request object to be validated.
+    this.upload_middleware = null; // A Multer configuration.
+
+    // Copy the config into this instance.
+    Object.assign(this, config);
+
+    // Make sure the authenticator is valid and prepare to validate the request for it.
+    if (!(this.authenticator in authenticators)) {
+      throw new Error(`Unknown authenticator: ${this.authenticator}`);
+    }
+    this.request_schemas = this.request_schemas.concat(authenticators[this.authenticator].request_schemas);
+
+    // Set up the route-specific error handleri.
+    this.server.use(this.path, this.errorHandler.bind(this));
+
+    // Validate the request.
+    if (this.request_schemas.length > 0) {
+      this.server.use(this.path, this.validate.bind(this));
+    }
+
+    // Authenticate the user.
+    if (this.authenticator !== 'none') {
+      this.server.use(this.path, authenticators[this.authenticator].middleware.bind(this));
+    }
+
+    // Wrap the endpoint in a managed transaction, if requested.
+    let wrapped_endpoint = this.endpoint;
+    if (this.transaction) {
+      wrapped_endpoint = async (req, res, next) => {
+        try {
+          await this.database.sequelize.transaction(async transaction => {
+            return this.endpoint(req, res, next, transaction);
+          });
+        } catch (err) {
+          this.log.error(err, 'Rolled back transaction in endpoint.');
+          return next(err);
+        }
+      };
+    }
+
+    // Enable the route and bind our endpoint, including upload middleware if applicable.
+    if (typeof this.upload_middleware === 'function') {
+      this.server[this.method](this.path, this.upload_middleware, wrapped_endpoint.bind(this));
+    } else {
+      this.server[this.method](this.path, wrapped_endpoint.bind(this));
+    }
+  }
+
+  /**
+   * A placeholder endpoint that does nothing. This method should be extended by the inheriting class.
+   *
+   * @param {object} req Express request.
+   * @param {object} res Express response.
+   * @param {function} next Express callback.
+   * @returns {null} No value.
+   */
   async endpoint(req, res, next) {
     return res.json({});
   }
 
+  /**
+   * The validate function is provided as an optional middleware. The constructor will install it if
+   * at least one request schema is passed. Note that authentication middleware may contribute request
+   * schemas of their own if used, so a custom extension should take these into account.
+   *
+   * @param {object} req Express request.
+   * @param {object} res Express response.
+   * @param {function} next Express callback.
+   * @returns {null} No value.
+   */
   async validate(req, res, next) {
     try {
       for (const request_schema of this.request_schemas) {
@@ -31,70 +121,27 @@ class Endpoint {
     return next();
   }
 
+  /**
+   * A default error handler to catch exceptions thrown by this Endpoint's middleware.
+   *
+   * @param {error} err Error object.
+   * @param {object} req Express request.
+   * @param {object} res Express response.
+   * @param {function} next Express callback.
+   * @returns {null} No value.
+   */
   async errorHandler(err, req, res, next) {
     this.log.warn(err, 'Error in route.', { path: req.path, ip: req.ip, hostname: req.hostname });
     return res.status(400).json({ err });
   }
 
-  constructor(server, database, logger, config) {
-    // Make these handles accessible.
-    this.server = server;
-    this.database = database;
-    this.models = database.models;
-    this.Op = database.sequelize.Op;
-    this.log = logger ? logger.child(__filename) : console;
-
-    // Default configuration values.
-    this.method = 'get';
-    this.path = '';
-    this.scopes = [];
-    this.transaction = false;
-    this.authenticator = 'none';
-    this.request_schemas = []; // An array of maps to fields in the request object to be validated.
-    this.upload_middleware = null; // A Multer configuration.
-
-    // Copy the config into this instance.
-    Object.assign(this, config);
-
-    // Make sure the authenticator is valid and prepare to validate the request for it.
-    if (!(this.authenticator in authenticators)) throw new Error(`Unknown authenticator: ${this.authenticator}`);
-    this.request_schemas = this.request_schemas.concat(authenticators[this.authenticator].request_schemas);
-
-    // Set up the route-specific error handleri.
-    this.server.use(this.path, this.errorHandler.bind(this));
-
-    // Validate the request.
-    if (this.request_schemas.length > 0) this.server.use(this.path, this.validate.bind(this));
-
-    // Authenticate the user.
-    if (this.authenticator !== 'none')
-      this.server.use(this.path, authenticators[this.authenticator].middleware.bind(this));
-
-    // Wrap the endpoint in a managed transaction, if requested.
-    let wrapped_endpoint = this.endpoint;
-    if (this.transaction) {
-      wrapped_endpoint = async (req, res, next) => {
-        try {
-          await this.database.sequelize.transaction(async transaction => {
-            return this.endpoint(req, res, next, transaction);
-          });
-        } catch (err) {
-          this.log.error(err, 'Rolled back transaction in endpoint.');
-          next(err);
-        }
-      };
-    }
-
-    // Enable the route and bind our endpoint, including upload middleware if applicable.
-    if (typeof this.upload_middleware === 'function') {
-      this.server[this.method](this.path, this.upload_middleware, wrapped_endpoint.bind(this));
-    } else {
-      this.server[this.method](this.path, wrapped_endpoint.bind(this));
-    }
-  }
-
+  /**
+   * Override toString for more pleasant logging.
+   *
+   * @returns {string} No value.
+   */
   toString() {
-    return `${this.method} ${this.path} (${this.scopes})`;
+    return JSON.stringify()`${this.method} ${this.path} (${this.scopes})`;
   }
 }
 
